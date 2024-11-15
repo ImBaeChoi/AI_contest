48b5f322561d6ab432fb1da5c82a6149beb63995
ImBaeChoi <swchoiimbae@gmail.com>
2024-11-15 17:19:04 +0900

    2024.11.15.17.19

--- test2.py
+++ test2.py
@@ -2,16 +2,17 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QPushButton
 from PyQt5.QtCore import Qt, QPropertyAnimation, QRect, QRectF
 from PyQt5 import uic
-from PyQt5.QtGui import QMovie, QIcon, QPainterPath, QRegion
+from PyQt5.QtGui import QMovie, QPainterPath, QRegion
 
 form_class = uic.loadUiType("test2.ui")[0]  # 메인 UI 로드
 dialog_form_class = uic.loadUiType("new_dialog.ui")[0]  # 옵션 UI 로드
+exit_dialog_form_class = uic.loadUiType("exit.ui")[0]  # 종료 UI 로드
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+
         # 루프 애니메이션 설정
         self.movie = QMovie("ani.gif")
         self.loopAni.setMovie(self.movie)
@@ -34,30 +35,34 @@ class WindowClass(QMainWindow, form_class):
         # 시그널 연결
         self.hidden_mic_btn.clicked.connect(self.loopAniFuction)
         self.answer_edit.textChanged.connect(self.adjust_dialog_size)
-        
-        # toolButton 클릭 시 새 창 열기
-        self.toolButton.clicked.connect(self.open_new_dialog)
 
-    def loopAniFuction(self):  # 애니메이션 실행 및 ScrollArea 토글 함수
+        # toolButton 클릭 시 새 창 열기
+        self.toolButton.clicked.connect(lambda: self.open_new_dialog(CustomDialog))
+
+        # exitButton이 있는 경우에만 연결
+        if hasattr(self, "exitButton"):
+            self.exitButton.clicked.connect(lambda: self.open_new_dialog(ExitDialog))
+
+    def loopAniFuction(self):
         if self.movie.state() == QMovie.Running:
             self.movie.stop()
             self.scrollArea.setVisible(False)
-        else: 
+        else:
             self.movie.start()
             self.scrollArea.setVisible(True)
 
-    def apply_rounded_corners(self, radius=20):  # 둥근 모서리 적용 함수
+    def apply_rounded_corners(self, radius=20):
         rect = QRectF(0, 0, self.width(), self.height())
         path = QPainterPath()
         path.addRoundedRect(rect, radius, radius)
         region = QRegion(path.toFillPolygon().toPolygon())
         self.setMask(region)
 
-    def resizeEvent(self, event):  # 창 크기 변경 시 둥근 모서리 재적용
+    def resizeEvent(self, event):
         super().resizeEvent(event)
         self.apply_rounded_corners()
 
-    def move_to_bottom_right(self):  # 우측 하단 고정 함수
+    def move_to_bottom_right(self):
         screen_geometry = QApplication.primaryScreen().availableGeometry()
         screen_width = screen_geometry.width()
         screen_height = screen_geometry.height()
@@ -69,7 +74,7 @@ class WindowClass(QMainWindow, form_class):
         y = screen_height - window_height
         self.move(x, y)
 
-    def adjust_dialog_size(self):  # 텍스트 길이에 따른 창 높이 조정 및 우측 하단 고정 함수
+    def adjust_dialog_size(self):
         document_height = self.answer_edit.document().size().height()
         new_height = max(544, min(int(document_height) + 250, 800))
         current_size = self.size()
@@ -85,10 +90,9 @@ class WindowClass(QMainWindow, form_class):
 
         self.move(x, y)
 
-    def open_new_dialog(self):  # 옵션 창을 여는 함수
-        self.dialog = CustomDialog()
-        self.dialog.show()
-        self.dialog.exec_()
+    def open_new_dialog(self, dialog_class):
+        dialog = dialog_class()
+        dialog.exec_()
 
 # 옵션 창 클래스
 class CustomDialog(QDialog, dialog_form_class):
@@ -106,18 +110,18 @@ class CustomDialog(QDialog, dialog_form_class):
         self.toggle_btn1.clicked.connect(lambda: self.toggle(self.toggle_btn1, self.btn1_btn))
         self.toggle_btn2.clicked.connect(lambda: self.toggle(self.toggle_btn2, self.btn2_btn))
 
-    def apply_rounded_corners(self, radius=20):  # 둥근 모서리 적용 함수
+    def apply_rounded_corners(self, radius=20):
         rect = QRectF(0, 0, self.width(), self.height())
         path = QPainterPath()
         path.addRoundedRect(rect, radius, radius)
         region = QRegion(path.toFillPolygon().toPolygon())
         self.setMask(region)
 
-    def toggle(self, button, sbtn): #옵션 버튼 함수
+    def toggle(self, button, sbtn):
         y_position = sbtn.geometry().top()
 
         if button.isChecked():
-            button.setText("ON    ")
+            button.setText("ON")
             button.setStyleSheet("""
                 QPushButton {
                     background-color: #8BC34A;
@@ -127,9 +131,9 @@ class CustomDialog(QDialog, dialog_form_class):
                 }
             """)
             start_pos = sbtn.geometry().left()
-            end_pos = 88  # 스위치 버튼이 오른쪽으로 이동할 위치 (ON 상태)
+            end_pos = 88  # 오른쪽으로 이동
         else:
-            button.setText("      OFF")
+            button.setText("OFF")
             button.setStyleSheet("""
                 QPushButton {
                     background-color: lightgray;
@@ -139,13 +143,31 @@ class CustomDialog(QDialog, dialog_form_class):
                 }
             """)
             start_pos = sbtn.geometry().left()
-            end_pos = 38 
+            end_pos = 38  # 왼쪽으로 이동
 
         self.animation = QPropertyAnimation(sbtn, b"geometry")
-        self.animation.setDuration(300)  # 애니메이션 지속 시간
+        self.animation.setDuration(300)
         self.animation.setStartValue(QRect(start_pos, y_position, 40, 40))
         self.animation.setEndValue(QRect(end_pos, y_position, 40, 40))
         self.animation.start()
+
+class ExitDialog(QDialog, exit_dialog_form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # 상단 바 제거
+        self.setWindowFlag(Qt.FramelessWindowHint)
+
+        # 둥근 모서리 적용
+        self.apply_rounded_corners()
+
+    def apply_rounded_corners(self, radius=20):
+        rect = QRectF(0, 0, self.width(), self.height())
+        path = QPainterPath()
+        path.addRoundedRect(rect, radius, radius)
+        region = QRegion(path.toFillPolygon().toPolygon())
+        self.setMask(region)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
